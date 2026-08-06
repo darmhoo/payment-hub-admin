abb743e01a4b50d498ba2968d4a192666d7520e3
Omodamola Oladeji <darmhoo@hotmail.com>
2026-08-06 13:19:13 +0100

    should work now

--- lib/api.ts
+++
@@ -1,8 +0,0 @@
-export function getApiBaseUrl() {
-  return (process.env.NEXT_API_URL ?? "http://localhost:8080").replace(/\/$/, "");
-}
-
-export function getApiUrl(path: string) {
-  const normalizedPath = path.startsWith("/") ? path : `/${path}`;
-  return `${getApiBaseUrl()}${normalizedPath}`;
-}
